be80aebabf5757aedfe8084532dbb6d16aeb2ef5
John Ko <johnko426@gmail.com>
2016-02-20 01:25:59 -0800

    Initial Commit

---
+++ IOS8swiftTakePhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS8swiftTakePhoto
+//
+//  Created by John Ko on 2/20/16.
+//  Copyright © 2016 John Ko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
