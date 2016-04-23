9a2369c454b87e0de6c2f93ca80c09fb66254469
John Ko <johnko426@gmail.com>
2016-04-23 11:10:42 -0700

    WIP

---
+++ IOS8swiftTakePhoto/Request.swift
@@ -0,0 +1,9 @@
+//
+//  API.swift
+//  IOS8swiftTakePhoto
+//
+//  Created by John Ko on 2/21/16.
+//  Copyright © 2016 John Ko. All rights reserved.
+//
+
+import Foundation
---
+++ IOS8swiftTakePhoto/TaggingAPI.swift
@@ -0,0 +1,9 @@
+//
+//  TaggingAPI.swift
+//  IOS8swiftTakePhoto
+//
+//  Created by John Ko on 2/20/16.
+//  Copyright © 2016 John Ko. All rights reserved.
+//
+
+import Foundation
